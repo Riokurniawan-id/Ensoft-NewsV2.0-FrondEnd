e7493da88549428e081d453191eba3eaffd485ef
Riokurniawan-id <riosayang911@gmail.com>
2024-01-28 14:31:04 +0700

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+import Profile from "@/asset-dummy/logo-strapi.jpg";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header class="bg-gray-50">
+      <div class="mx-auto max-w-screen-xl px-4 pb-8 sm:pb-6 lg:px-8">
+        <div class="mt-8">
+          <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
+            Welcome To Ensoft News
+          </h1>
+
+          <p class="mt-1.5 text-sm text-gray-500">
+            Your website has seen a 52% increase in traffic in the last month.
+            Keep it up! 🚀
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
