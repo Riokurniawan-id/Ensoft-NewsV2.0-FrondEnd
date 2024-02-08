ca5e61f81c5b6a13077b5bbbc35d551a6b5a38bb
Riokurniawan-id <riosayang911@gmail.com>
2024-02-08 20:03:57 +0700

    add dummy logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-
+import Head from "next/head";
+import Logo from "@/asset-dummy/logo-strapi.jpg";
 import LayoutIndex from "@/layout/LayoutIndex";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,6 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ensoft</title>
+        <link rel="icon" href="/logo-strapi.jpg" />
+      </Head>
       <LayoutIndex />
     </>
   );
